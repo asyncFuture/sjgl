ff63e58080bf1d9a5b943f4c0ead42e13cb36792
ASYNC <asyncfuture@gmail.com>
2025-02-01 06:01:25 +0100

    bugfix: corrected texture cords to prevent out-of-bounds access

--- src/main/java/me/async/sjgl/Texture.java
+++ src/main/java/me/async/sjgl/Texture.java
@@ -43,10 +43,12 @@ public class Texture {
     }
 
     public Vector4f texture(Vector2f uv) {
-        int u = (int) (uv.x * (width - 1));
-        int v = (int) (uv.y * (height - 1));
+        int u = Math.max(0, Math.min((int) (uv.x * (width - 1)), width - 1));
+        int v = Math.max(0, Math.min((int) (uv.y * (height - 1)), height - 1));
+
         return SJGL.rgba(getPixel(u, v));
     }
+
 
     public int getPixel(int x, int y) {
         return data[x + y * width];
