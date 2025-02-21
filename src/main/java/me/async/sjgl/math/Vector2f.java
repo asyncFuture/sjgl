d7626779a9a2e1ce1520053d7f42709586435708
ASYNC <asyncfuture@gmail.com>
2025-02-21 15:33:47 +0100

    added new constructor x=(xy)

--- src/main/java/me/async/sjgl/math/Vector2f.java
+++ src/main/java/me/async/sjgl/math/Vector2f.java
@@ -2,10 +2,14 @@ package me.async.sjgl.math;
 
 public class Vector2f {
 
-    public float x,y;
+    public float x, y;
 
     public Vector2f(float x, float y) {
         this.x = x;
         this.y = y;
+    }
+
+    public Vector2f(float x) {
+        this(x, x);
     }
 }
